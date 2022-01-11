753da5da2f65e7f5e097506b85133527df358420
Amir Moradi <amirhmoradi@users.noreply.github.com>
2022-01-11 13:20:48 +0100

    fix: new release, all working, added multiple config env vars, see mongodb-brain-evo.js Configuration section

--- src/mongodb-brain-evo.js
+++ src/mongodb-brain-evo.js
@@ -1,12 +1,17 @@
 // Description:
-//   hubot-mongodb-brain
-//   support MongoLab and MongoHQ on heroku.
+//   hubot-mongodb-brain-evo
+//   supports MongoLab and MongoHQ on heroku for Mongo URLs.
+//   supports custom collection and db name.
 //
 // Dependencies:
 //   "mongodb": "*"
 //
 // Configuration:
-//   MONGODB_URL or MONGOLAB_URI or MONGOHQ_URL or 'mongodb://localhost:27017/hubot'
+// ( VAR || VAR2 ... || default_value_if_not_set )
+//   MONGODB_URL || MONGOLAB_URI || MONGOHQ_URL || 'mongodb://localhost:27017/hubot'
+//   MONGODB_SAVE_INTERVAL || 10
+//   MONGODB_BRAIN_COLLECTION || brain
+//   MONGODB_DB_NAME || <VAL_EXTRACTED_FROM_MONGODB_URL_PATH> || hubot
 //
 // Author:
 //   Sho Hashimoto <hashimoto@shokai.org>
@@ -18,6 +23,19 @@
 const { MongoClient } = require('mongodb');
 const Url = require('url');
 
+const getCircularReplacer = (key, value) => {
+    const seen = new WeakSet();
+    return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+        return;
+        }
+        seen.add(value);
+    }
+    return value;
+    };
+};
+
 class MongoDB {
   constructor(robot) {
     this.robot = robot;
@@ -28,23 +46,17 @@ class MongoDB {
       process.env.MONGOHQ_URL ||
       'mongodb://localhost:27017/hubot';
 
-    this.brainCollection = process.env.MONGODB_BRAIN_COLLECTION || 'brain'
-
+    this.brainCollection = process.env.MONGODB_BRAIN_COLLECTION || 'brain';
+    
     const urlInfo = Url.parse(this.url);
-    this.dbName = urlInfo.pathname ? urlInfo.pathname.replace('/', '') : 'hubot';
+    this.dbName = process.env.MONGODB_DB_NAME || (urlInfo.pathname ? urlInfo.pathname.replace('/', '') : 'hubot');
   }
 
   async _connect() {
     this.client = await MongoClient.connect(this.url);
     this.db = this.client.db(this.dbName);
     try {
-      this.db.createCollection(this.brainCollection, {strict: false})
-              .then(collection => {
-                this.collection = collection;
-              })
-              .catch(reason => {
-                this.robot.logger.error('MongoDB Create Collection Error:' + reason);
-              });
+        this.collection = this.db.collection(this.brainCollection);
     } catch (e) {
       this.robot.logger.error('MongoDB Create Collection Error:' + e);
     }
@@ -60,38 +72,39 @@ class MongoDB {
       if (err) {
         throw err;
       }
-      this.robot.brain.mergeData(
-        docs.reduce((obj, doc) => {
-          obj[doc.key] = doc.value;
-          return obj;
-        }, {})
-      );
-      this.robot.logger.info('MongoDB loaded');
+      
+      const brainDbDocs = docs.reduce((obj, doc) => {
+        obj[doc.key] = doc.value;
+        return obj;
+      }, {});
+      this.robot.brain.mergeData(brainDbDocs);
+
       this.robot.brain.resetSaveInterval(this.resetSaveInterval);
       this.robot.brain.setAutoSave(true);
     });
   }
 
   async save(data) {
+    this.robot.logger.info('MongoDB save caught');
+
     const dataKeys = Object.keys(data || {});
-    this.robot.logger.info('MongoDB save caught:' + dataKeys, data);
     if (dataKeys.length > 0) {
       this.robot.brain.setAutoSave(false);
 
       const bulkWriteOperations = [];
       dataKeys.forEach((key) => {
-        bulkWriteOperations.push({updateOne: { filter: {key}, update: {$set: data[key]}, upsert: true}});
+          if( typeof data[key] !== "undefined" ){
+            // data[key] = JSON.stringify(data[key], getCircularReplacer(),2); // Use this if your data is not standard JSON.
+            bulkWriteOperations.push({updateOne: { filter: {key}, update: {$set: {"key": key, "value": data[key]}}, upsert: true}});
+          }
       });
-      this.robot.logger.info('MongoDB Operations:', bulkWriteOperations);
-      await this.collection.bulkWrite(bulkWriteOperations)
+      this.collection.bulkWrite(bulkWriteOperations)
                             .then( updateResults => {
-                              this.robot.logger.info('MongoDB updated records:' + updateResults);
+                              this.robot.logger.info('MongoDB updated records: ' + updateResults.modifiedCount);
                             })
                             .catch (reason => {
-                              this.robot.logger.error('MongoDB update error:' + reason);
+                              this.robot.logger.error('MongoDB update error: ' + reason);
                             });
-      
-      
       this.robot.brain.setAutoSave(true);
     }
   }
